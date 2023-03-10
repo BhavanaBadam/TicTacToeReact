d25eff8744cb9c32aa9429151ebc6374abc545df
Bhavana Badam <bhavana.badam@oracle.com>
2023-03-10 21:46:33 +0530

    Basic

--- src/assignment1.jsx
+++ src/assignment1.jsx
@@ -17,23 +17,58 @@ import { useState } from "react";
 
 export const Assignment1 = () => {
     const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("");
+    const [nationality, setNationality] = useState("India");
+    const [isMarried, setIsMarried] = useState("");
+    const [hasChildren, setHasChildren] = useState("");
+    const [hasSiblings, setHasSiblings] = useState("");
 
     const submitFinalForm = () => {
+        if (isNaN(age)) {
+            alert("Age is Invalid");
+            return;
+        }
         const formValue = {
             name,
-            age: 18,
-            gender: 'X',
-            nationality: 'in',
-            married: true,
-            hasSiblings: true,
-            hasChildren: false
+            age,
+            gender,
+            nationality,
+            "isMarried" : isMarried === true ? true : false,
+            "hasChildren" : hasChildren === true ? true : false,
+            "hasSiblings" : hasSiblings === true ? true : false,
         };
         console.log("form submit", formValue);
         alert(JSON.stringify(formValue));
     };
 
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+
     return <div>
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <div> Name: <input value={name} onChange={e => setName(e.target.value)} /></div>
+        <div> Age:  <input value={age} onChange = {e => setAge(e.target.value)} /></div>
+        <div>
+            Gender : <br/>
+          <input type="radio" name="gender" value="male"  onChange={e => handleGenderChange(e)}/>
+          <label >Male</label> <br/>
+          <input type="radio" name="gender" value="female" onChange={handleGenderChange}/>
+          <label > Female</label> <br/>
+        </div>
+        <div>
+            Nationality :
+            <select value = {nationality} onChange = {e => setNationality(e.target.value) }>
+                <option value="India">India</option>
+                <option value="Aus">Aus</option>
+                <option value="UK">UK</option>
+                <option value="USA">USA</option>
+            </select>
+        </div>
+        <div> isMarried : <input type="checkbox" checked={isMarried} onChange={e => setIsMarried(e.target.checked)} /></div>
+        <div> hasChildren : <input type="checkbox" checked={hasChildren} onChange={e => setHasChildren(e.target.checked)} /></div>
+        <div> hasSiblings : <input type="checkbox" checked={hasSiblings} onChange={e => setHasSiblings(e.target.checked)} /></div>
+
         <button onClick={submitFinalForm}>Submit!</button>
     </div>;
 }
