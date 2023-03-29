0ec018a57af51b3a79022f323c0a487df5a0b311
Bhavana Badam <bhavana.badam@oracle.com>
2023-03-29 22:02:57 +0530

    temp2

--- src/assignment2/newPost.jsx
+++ src/assignment2/newPost.jsx
@@ -1,16 +1,29 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const NewPost = () => {
     const [postName, setPostName] = useState("");
     const [selectedFile, setSelectedFile] = useState(null);
     const [msg, setMsg] = useState("");
     const [uploadImage, setUploadImage] = useState(true);
+    const [posts, setPosts] = useState([]);
+    const [isError, setIsError] = useState(false);
 
+    useEffect(() => {
+       fetch("/posts")
+        .then((response) => response.json())
+        .then((data) => {
+          setPosts(data.posts);
+        })
+        .catch((error) => console.error("Error while fetching data:", error));
+    }, []);
+
+    console.log(posts);
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
 
     const handleSubmit = (event) => {
+
         event.preventDefault();
         
       
@@ -19,20 +32,15 @@ const NewPost = () => {
         formData.append("image", selectedFile);
 
         setMsg("Uploading..!");
-        fetch('/posts')
-          .then(response => response.json())
-          .then(data => {
-            console.log(data.posts);
-            data.posts.map(element => {
-              if (element === postName) {
-                setMsg("Duplicate File Name");
-                setUploadImage(false);
-                console.log("i'm Here");
-                throw new Error('HTTP error! status: 409')
-              }
-            });
-          })
-          .catch(error => console.error('Error while fetching data:', error));
+
+        posts.map(element => {
+          if (element === postName) {
+            setMsg("Duplicate File Name");
+            setUploadImage(false);
+            console.log("i'm Here");
+            throw new Error('HTTP error! status: 409')
+          }
+        });
 
         console.log(uploadImage + "1");
         
@@ -54,6 +62,7 @@ const NewPost = () => {
             console.error('Error while uploading file:', error);
           })
         }
+
     };
     return <div>
     <form onSubmit={handleSubmit}>
