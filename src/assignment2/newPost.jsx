1a6ac4a4e100a2e3d9163ade07363d9c73d501bb
Bhavana Badam <bhavana.badam@oracle.com>
2023-03-29 21:22:17 +0530

    temp

--- src/assignment2/newPost.jsx
+++ src/assignment2/newPost.jsx
@@ -3,9 +3,8 @@ import {useState} from "react";
 const NewPost = () => {
     const [postName, setPostName] = useState("");
     const [selectedFile, setSelectedFile] = useState(null);
-    const [uploading, setUploading] = useState(false);
-    const [success, setSuccess] = useState(false);
-    const [errorMsg, setErrorMsg] = useState("");
+    const [msg, setMsg] = useState("");
+    const [uploadImage, setUploadImage] = useState(true);
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
@@ -18,40 +17,50 @@ const NewPost = () => {
         const formData = new FormData();
         formData.append("postName", postName)
         formData.append("image", selectedFile);
-        setUploading(true);
-        setSuccess(false);
-        fetch("/post/new", {
-          method: "POST",
-          body: formData
-        })
-        .then(response => {
-          if (!response.ok) {
-            setErrorMsg("Duplicate FileName");
-            console.log("I am here");
-            throw new Error(`HTTP error! status: ${response.status}`);
-          } else {
-            setSuccess(true);
-          }
-        })
-        .then(data => {
-          console.log('Upload successful!', data);
-        })
-        .catch(error => {
-          console.error('Error while uploading file:', error);
-        })
-        .finally(() => {
-          setUploading(false);
-        });
-    };
 
+        setMsg("Uploading..!");
+        fetch('/posts')
+          .then(response => response.json())
+          .then(data => {
+            console.log(data.posts);
+            data.posts.map(element => {
+              if (element === postName) {
+                setMsg("Duplicate File Name");
+                setUploadImage(false);
+                console.log("i'm Here");
+                throw new Error('HTTP error! status: 409')
+              }
+            });
+          })
+          .catch(error => console.error('Error while fetching data:', error));
+
+        console.log(uploadImage + "1");
+        
+        if(uploadImage) {
+          fetch("/post/new", {
+            method: "POST",
+            body: formData
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            } 
+            setMsg("Uploaded");
+          })
+          .then(data => {
+            console.log('Upload successful!', data);
+          })
+          .catch(error => {
+            console.error('Error while uploading file:', error);
+          })
+        }
+    };
     return <div>
     <form onSubmit={handleSubmit}>
 	    <div> PostName: <input value={postName} onChange={e => setPostName(e.target.value)} /></div>
         <div> UploadPost: <input type="file" name="file" onChange={handleFileChange} /></div>
-        <button type="submit">Submit</button>
-        {uploading && <p>Uploading...</p>}
-      {success && <p>Upload successful!</p>}
-      {errorMsg}
+        <button type="submit">Submit</button> 
+        <h3>{msg}</h3>
     </form>
 	</div>;
 }
