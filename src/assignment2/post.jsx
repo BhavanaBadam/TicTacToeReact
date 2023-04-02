eead4a26848df71e40738a645c957c6ec79f951c
Bhavana Badam <bhavana.badam@oracle.com>
2023-04-02 21:54:16 +0530

    Add comments

--- src/assignment2/post.jsx
+++ src/assignment2/post.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import { useParams, Link } from 'react-router-dom';
+const Buffer = require('buffer').Buffer;
 
 const usePost = (postName) => {
     const [post, setPost] = useState(null);
@@ -24,9 +25,49 @@ const usePost = (postName) => {
     return {post, isError, isLoading: (!isError && (post == null))};
 }
 
+const useComments = (postName) => {
+	const [existingComment, setExistingComment] = useState("");
+	useEffect(() => {
+		const comments = async(postName) => {
+			const resp = await fetch(`/post/${postName}/comments`);
+			const json = await resp.json();
+			const bufferArray = Buffer.from(json.postComments.data);
+			const lines = bufferArray.toString('utf8').split('\n');
+			console.log(lines);
+			setExistingComment(lines);
+		}
+		comments(postName);
+	}, [postName]);
+	//console.log("comments after setting: " + existingComment);
+	return existingComment;
+}
+
+const handleSubmit = async(event,newComment,postName) => {
+	event.preventDefault();
+	const formData = new FormData();
+	formData.append("new_comment", newComment);
+	const response = await fetch(`/post/${postName}/comments`, {
+	  method: "POST",
+	  body: formData
+	});
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	} 
+	console.log("successfull");
+	//return useComments(postName);
+};
+
+
+
 const Post = () => {
     const {postName} = useParams();
-    const {post, isLoading, isError} = usePost(postName);
+	const {post, isLoading, isError} = usePost(postName);
+	const existingComment = useComments(postName);
+	const [newComment, setNewComment] = useState("");
+
+	//console.log("imageUrl: " + post);
+	//console.log("existingComments: " + existingComment);
+
     return <>
 	       <Link to="/posts">Back</Link>
 	       {!!isLoading && <div>Loading...</div>}
@@ -34,6 +75,14 @@ const Post = () => {
 	       {!isLoading && !isError && <ul>
 	       <h3>{postName}</h3>
 	       <img src={post} />
+		   <h3>Comments : </h3>
+		   <ul>
+	    	{existingComment.map(comment => <li key={comment}>{comment}</li>)}
+	    	</ul>
+			<form onSubmit={e => handleSubmit(e,newComment,postName)}>
+	    		<input value={newComment} placeholder="Comment here..." onChange={e => setNewComment(e.target.value)} />
+        		<button type="submit">Submit</button> 
+    		</form>
 	       </ul>}
 	   </>;
 }
--- src/server.js
+++ src/server.js
@@ -11,7 +11,15 @@ const getPosts = () => {
 }
 
 const getPost = (postName) => {
+    //console.log("getPOst:" + postName);
     return fs.readFileSync(`${__dirname}/assignment2/posts/${postName}`);
+}
+
+const getPostComments = (postName) => {
+  //console.log(postName);
+  const filename = postName + ".txt";
+  //console.log(filename);
+  return fs.readFileSync(`${__dirname}/assignment2/comments/${postName}.txt`);
 }
 
 app.get("/posts", (req, res) => {
@@ -26,6 +34,13 @@ app.get("/post/:postName", (req, res) => {
     res.end(postData);
 });
 
+app.get("/post/:postName/comments", (req, res) => {
+  //res.writeHead(200);
+  const {postName} = req.params;
+  const postComments = getPostComments(postName);
+  res.json({postComments});
+});
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, `${__dirname}/assignment2/posts`);
@@ -38,9 +53,25 @@ const storage = multer.diskStorage({
   const upload = multer({ storage: storage });
   
   app.post("/post/new", upload.single("image"), (req, res) => {
+    fs.writeFile(`${__dirname}/assignment2/comments/${req.body.postName}.txt`, '', function (err) { 
+      if (err) throw err;
+      console.log(`File ${req.body.postName}.txt created!`);
+    });
     console.log(req.body.postName);
     res.sendStatus(200);
   });
+
+  const commentUpload = multer();
+  app.post("/post/:postName/comments", commentUpload.none(), (req, res) => {
+    const {postName} = req.params;
+    console.log(req.body);
+    const comments = req.body.new_comment;
+    fs.appendFile(`${__dirname}/assignment2/comments/${postName}.txt`, comments, (err) => {
+      if (err) throw err;
+      console.log('Data appended to file!');
+    });
+    res.sendStatus(200);
+  })
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
